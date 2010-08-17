c04ad13fbc39ca916c457e8c392dce853f1c4e59
windkit <windkithk@gmail.com>
2010-08-17 22:34:36 +0800

    Odometry PID v1 keil compiled
    Signed-off-by: windkit <windkithk@gmail.com>

--- code/RAL/ral_odometry.h
+++ code/RAL/ral_odometry.h
@@ -1,11 +1,11 @@
 #ifndef __RAL_ODOMETRY_H
 #define __RAL_ODOMETRY_H
 
-#define ENCODER_COUNT	  76000.0;  //count per revolution
-#define ENCODER_L_RADIUS  4.75;	    //cm
-#define ENCODER_R_RADIUS  4.75;	    //cm
+#define ENCODER_COUNT	  76000.0 	//count per revolution
+#define ENCODER_L_RADIUS  4.75	    //cm
+#define ENCODER_R_RADIUS  4.75	    //cm
 #define ENCODER_DISTANCE  46.0	    //Distance between 2 encoders (cm)
-#define PI		  	3.14159265
+#define PI	  		3.14159265
 #define PI_180		0.017453292519943295769236907684886
 #define PI_2		1.5707963267948966192313216916398
 #define C180_PI		57.295779513082320876798154814105
